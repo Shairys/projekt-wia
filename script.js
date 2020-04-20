b8e25edf9748a816e47d851a7d87cce46d3b683f
Avengens <aleksander.czerwionka@gmail.com>
2020-04-20 02:48:25 +0200

    animation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function(){
     
+    
+    
     function showButton(){ //Pokazuje i chowa guzik podczas scrollowania
         var offset = this.pageYOffset; //Offset Y strony
         var button = document.querySelector("#backToTop");
@@ -7,9 +9,27 @@ window.onload = function(){
             button.style.display = "block";
         else
             button.style.display = "none";
+        button.querySelector("p").innerHTML = offset;
+    }
+    
+    function animationSlide(){
+        var offset = this.pageYOffset;
+        if(offset >= 1900 && offset <= 3200){
+            document.querySelector("#first-article").style.visibility = "visible";
+            setTimeout(function(){document.querySelector("#first-article").style.left = 0;},100);
+        }
+        if(offset >= 3000 && offset <= 4100){
+            document.querySelector("#second-article").style.visibility = "visible";
+            setTimeout(function(){document.querySelector("#second-article").style.left = 0;},100);
+        }
+        if(offset >= 4300 && offset <= 5400){
+            document.querySelector("#third-article").style.visibility = "visible";
+            setTimeout(function(){document.querySelector("#third-article").style.left = 0;},100);
+        }
     }
 
     window.addEventListener('scroll', showButton);
+    window.addEventListener('scroll', animationSlide);
     document.querySelector("#backToTop").addEventListener('click', function(){
         window.scrollBy(0, -window.pageYOffset); //scrolluje w górę o offsetY strony
     });
